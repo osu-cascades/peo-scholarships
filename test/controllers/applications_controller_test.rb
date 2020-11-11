73c4f04239ac2d09ede0e1619cb65f64d206ffcd
Yong Bakos <ybakos@humanoriented.com>
2020-11-11 12:38:00 -0800

    Applications: Test applications controller.

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'requires user authentication' do
+    assert(defines_before_filter?(ApplicationsController, :authenticate_user!))
+  end
+
+  test 'redirects requests from unauthenticated sessions' do
+    # index
+    get applications_path
+    assert_redirected_to new_user_session_path
+
+  end
+
 end
