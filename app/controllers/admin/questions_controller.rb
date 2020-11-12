6e60388dbb144f3c7d5f684fa1773cc642ea9546
Yong Bakos <ybakos@humanoriented.com>
2020-11-11 23:47:56 -0800

    Question: Add create action.
    References #24.

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -8,13 +8,28 @@ class Admin::QuestionsController < Admin::AdminController
   def show; end
 
   def new
-    @scholarship = Scholarship.includes('questions').find(params[:scholarship_id])
+    @scholarship = Scholarship.find(params[:scholarship_id])
     @question = @scholarship.questions.build
   end
 
-  def create; end
+  def create
+    @scholarship = Scholarship.find(params[:scholarship_id])
+    @question = @scholarship.questions.build(question_params)
+    if @question.save
+      redirect_to [:admin, @scholarship, :questions], notice: 'Question added.'
+    else
+      render :new
+    end
+  end
+
   def edit; end
   def update; end
   def destroy; end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:prompt)
+  end
 
 end
