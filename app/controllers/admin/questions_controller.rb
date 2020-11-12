20b122ecc2ad1f3c5904cd5ec7eae69bba029dfe
Yong Bakos <ybakos@humanoriented.com>
2020-11-11 17:45:33 -0800

    Question: Add basic admin list view, scoped to scholarship.
    References #24.

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,6 +1,10 @@
 class Admin::QuestionsController < Admin::AdminController
 
-  def index; end
+  def index
+    @scholarship = Scholarship.includes('questions').find(params[:scholarship_id])
+    @questions = @scholarship.questions
+  end
+
   def show; end
   def new; end
   def create; end
