5cf9ad9cb70c259402be04754e6277e93f75b042
Yong Bakos <ybakos@humanoriented.com>
2020-11-16 15:57:19 -0800

    Routes: Remove main applications route.
    Applications will be scoped per scholarship, so let's remove this main route for now.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :new, :create, :destroy]
   end
 
-  get "applications" => "applications#index"
   resources :scholarships
 
   namespace :admin do
