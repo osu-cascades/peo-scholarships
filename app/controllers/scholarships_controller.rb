7bee8ab7483fbd11da76b4a5bdd585a221dbd62f
Taylor Slye <slyet@oregonstate.edu>
2020-11-16 14:58:20 -0800

    allow for creation of scholarships

--- app/controllers/scholarships_controller.rb
+++ app/controllers/scholarships_controller.rb
@@ -2,4 +2,14 @@ class ScholarshipsController < ApplicationController
 	def index
 		@scholarships = Scholarship.all
 	end
+
+	def new
+		@scholarship = Scholarship.new
+	end
+
+	def create
+		@scholarship = Scholarship.new(params.require(:scholarship).permit(:name))
+		@scholarship.save
+		redirect_to scholarships_path
+	end
 end
