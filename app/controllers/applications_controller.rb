10d4621588b9d880bce534d744ed961701ce1ad9
Taylor Slye <slyet@oregonstate.edu>
2020-11-23 15:11:26 -0800

    add create show and delete to scholarship application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,7 +4,32 @@ class ApplicationsController < ApplicationController
 	end
 
 	def create
-		@application = Application.new(user_id:current_user.id, scholarship_id: scholarship.id)
-		render scholarships_path
+		@scholarship = Scholarship.find(params[:scholarship_id])
+		@application = Application.new(user_id:current_user.id, scholarship_id: @scholarship.id)
+		if @application.save
+			redirect_to scholarship_application_path(@scholarship, @application), notice: 'Scholarship created.'
+		  else
+			render :new
+		  end
 	end
+
+	def new
+		@scholarship = Scholarship.find(params[:scholarship_id])
+		@application = Application.new(user_id:current_user.id, scholarship_id: @scholarship.id)
+	end
+
+	def show
+		@scholarship = Scholarship.find(params[:scholarship_id])
+		@application = Application.find(params[:id])
+	end
+
+	def destroy
+		@application = Application.find(params[:id])
+		if @application.destroy
+		  redirect_to scholarships_url, notice: 'Application deleted.'
+		else
+		  redirect_to scholarships_url, alert: 'Could not delete application.'
+		end
+	  end
+	
 end
