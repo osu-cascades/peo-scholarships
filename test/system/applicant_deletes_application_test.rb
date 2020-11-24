9c799025deabf6b3f3b61ae9b4d79a0e27881a20
Yong Bakos <ybakos@humanoriented.com>
2020-11-23 23:07:35 -0800

    Application: Test applicant deletes application.

--- test/system/applicant_deletes_application_test.rb
+++ test/system/applicant_deletes_application_test.rb
@@ -5,6 +5,10 @@ class ApplicantDeletesApplicaationTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
 
   test 'applicant deletes their application' do
-    skip
+    sign_in(users(:applicant))
+    application = applications(:first)
+    visit scholarship_application_path(application.scholarship, application)
+    click_on 'Delete'
+    assert_text 'Application deleted'
   end
 end
