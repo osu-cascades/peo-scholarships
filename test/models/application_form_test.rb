0e80c88e3bb9855f1ad0a7ce0e470eeb1519f1cc
Yong Bakos <ybakos@humanoriented.com>
2021-01-16 23:32:42 -0800

    ApplicationForm: Test associations and attributes.
    References #62.

--- test/models/application_form_test.rb
+++ test/models/application_form_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class ApplicationFormTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'belongs to an applicant' do
+    assert_respond_to(ApplicationForm.new, :applicant)
+  end
+
+  test 'belongs to a scholarship' do
+    assert_respond_to(ApplicationForm.new, :scholarship)
+  end
+
+  test 'has many answers' do
+    assert_respond_to(ApplicationForm.new, :answers)
+  end
+
+  test 'personal information attributes' do
+    application_form = ApplicationForm.new
+    assert_respond_to(application_form, :name)
+    assert_respond_to(application_form, :email)
+    assert_respond_to(application_form, :dob)
+    assert_respond_to(application_form, :address)
+    assert_respond_to(application_form, :phone_number)
+    assert_respond_to(application_form, :ages_of_children)
+    assert_respond_to(application_form, :marital_status)
+    assert_respond_to(application_form, :student_id)
+    assert_respond_to(application_form, :ssn)
+  end
+
 end
