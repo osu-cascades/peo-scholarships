a1bdda506b49ef392fd80a557efdea2986d8b2de
Yong Bakos <ybakos@humanoriented.com>
2021-01-17 00:08:18 -0800

    ApplicationFormsController: Scope loading to current user.
    In #45, we fixed an issue that allows a person to
    access another user's Application by changing the URL.
    
    Replicate this fix for ApplicationFormsController.
    
    References #58, #61, #62.

--- app/controllers/application_forms_controller.rb
+++ app/controllers/application_forms_controller.rb
@@ -1,8 +1,12 @@
+require 'marital_status'
+
 class ApplicationFormsController < ApplicationController
 
   def show
     @scholarship = Scholarship.find(params[:scholarship_id])
-    @application_form = ApplicationForm.find(params[:id])
+    @application_form = current_user.application_forms.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to scholarships_path, alert: 'There was a problem accessing this application form.'
   end
 
   def new
@@ -17,7 +21,7 @@ class ApplicationFormsController < ApplicationController
     @application_form.applicant = current_user
     @application_form.scholarship = @scholarship
     if @application.save
-      redirect_to scholarship_application_form_path(@scholarship, @application), notice: 'Application created.'
+      redirect_to scholarship_application_form_path(@scholarship, @application_form), notice: 'Application form created.'
     else
       @options_for_marital_status = MaritalStatus::STATUSES.map { |s| [s, s] }
       render :new
@@ -26,29 +30,35 @@ class ApplicationFormsController < ApplicationController
 
   def edit
     @scholarship = Scholarship.find(params[:scholarship_id])
-    @application_form = ApplicationForm.find(params[:id])
+    @application_form = current_user.application_forms.find(params[:id])
     @options_for_marital_status = MaritalStatus::STATUSES.map { |s| [s, s] }
+  rescue ActiveRecord::RecordNotFound
+    redirect_to scholarships_path, alert: 'There was a problem accessing this application form.'
   end
 
   def update
     @scholarship = Scholarship.find(params[:scholarship_id])
-    @application_form = ApplicationForm.find(params[:id])
-    if @application.update(application_params)
-      redirect_to scholarship_application_form_path(@scholarship, @application), notice: 'Application updated.'
+    @application_form = current_user.application_forms.find(params[:id])
+    if @application_form.update(application_params)
+      redirect_to scholarship_application_form_path(@scholarship, @application_form), notice: 'Application form updated.'
     else
       @options_for_marital_status = MaritalStatus::STATUSES.map { |s| [s, s] }
       render :edit
     end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to scholarships_path, alert: 'There was a problem accessing this application form.'
   end
 
   def destroy
-    application_form = ApplicationForm.find(params[:id])
+    application_form = current_user.application_forms.find(params[:id])
     application_form.destroy
     if application_form.destroyed?
-      redirect_to scholarships_url, notice: 'Application deleted.'
+      redirect_to scholarships_url, notice: 'Application form deleted.'
     else
-      redirect_to scholarships_url, alert: 'Could not delete application.'
+      redirect_to scholarships_url, alert: 'Could not delete application form.'
     end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to scholarships_path, alert: 'There was a problem accessing this application form.'
   end
 
   private
@@ -57,6 +67,5 @@ class ApplicationFormsController < ApplicationController
     params.require(:application).permit(:name, :dob, :email, :address,
       :phone_number, :ages_of_children, :marital_status, :student_id, :ssn)
   end
-
 
 end
