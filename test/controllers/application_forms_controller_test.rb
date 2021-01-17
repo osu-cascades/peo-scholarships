35ddaef1f7f97749ce4fdd13d7a1511adcd72863
Yong Bakos <ybakos@humanoriented.com>
2021-01-16 23:37:34 -0800

    ApplicationFormsController: Test access and authentication.

--- test/controllers/application_forms_controller_test.rb
+++ test/controllers/application_forms_controller_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ApplicationFormsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'requires user authentication' do
+    assert(defines_before_filter?(ApplicationFormsController, :authenticate_user!))
+  end
+
+  test 'redirects requests from unauthenticated sessions' do
+    # show
+    get scholarship_application_form_path(scholarship_id: 'fake', id: 'fake')
+    assert_redirected_to new_user_session_path
+    # new
+    get new_scholarship_application_form_path(scholarship_id: 'fake')
+    assert_redirected_to new_user_session_path
+    # edit
+    get edit_scholarship_application_form_path(scholarship_id: 'fake', id: 'fake')
+    assert_redirected_to new_user_session_path
+    # create
+    post scholarship_applications_path(scholarship_id: 'fake')
+    assert_redirected_to new_user_session_path
+    # update
+    patch scholarship_application_form_path(scholarship_id: 'fake', id: 'fake')
+    assert_redirected_to new_user_session_path
+    put scholarship_application_form_path(scholarship_id: 'fake', id: 'fake')
+    assert_redirected_to new_user_session_path
+    # destroy
+    delete scholarship_application_form_path(scholarship_id: 'fake', id: 'fake')
+    assert_redirected_to new_user_session_path
+  end
+
 end
