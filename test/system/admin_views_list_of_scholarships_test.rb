f5f106ca6204bca8735f41881568306a47c83e21
Yong Bakos <ybakos@humanoriented.com>
2021-01-20 13:17:46 -0800

    Scholarship: display indicator of published status.
    Add column to list view.
    Add small indicator to show view.
    Add unpublished scholarship to existing fixtures.
    Test.
    
    References #73.

---
+++ test/system/admin_views_list_of_scholarships_test.rb
@@ -0,0 +1,33 @@
+require 'application_system_test_case'
+
+class AdminViewsListOfScholarships < ApplicationSystemTestCase
+
+  include Devise::Test::IntegrationHelpers
+  include ActionView::RecordIdentifier
+
+  test 'admin creates a scholarship' do
+    sign_in(users(:admin))
+    visit admin_scholarships_path
+    assert_text 'Scholarships'
+    assert_text scholarships(:first).name
+  end
+
+  test 'admin sees indicator for published scholarship' do
+    sign_in(users(:admin))
+    visit admin_scholarships_path
+    published_scholarship = scholarships(:first)
+    within "##{dom_id(published_scholarship)}" do
+      assert_text 'Published'
+    end
+  end
+
+  test 'admin sees indicator for unpublished scholarship' do
+    sign_in(users(:admin))
+    visit admin_scholarships_path
+    unpublished_scholarship = scholarships(:unpublished)
+    within "##{dom_id(unpublished_scholarship)}" do
+      assert_text 'Not visible'
+    end
+  end
+
+end
