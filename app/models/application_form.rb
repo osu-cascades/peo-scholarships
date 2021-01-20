a68fc20b85c86f3816d3d751160acca91eb65016
Taylor Slye <slyet@oregonstate.edu>
2021-01-20 09:10:48 -0800

    remove application form model

--- app/models/application_form.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationForm < ApplicationRecord
-
-  belongs_to :applicant, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :scholarship
-  has_many :answers, dependent: :destroy
-
-end
