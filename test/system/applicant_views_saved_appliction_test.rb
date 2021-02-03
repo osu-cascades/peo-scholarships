6ff0cabd7c8c3d65c226c8fa21e5d52eee2d9c62
Yong Bakos <ybakos@humanoriented.com>
2021-02-03 14:21:00 -0800

    Application: Test for edit and delete link visibility.
    Applicants should only see edit/delete links for unsubmitted applications.
    
    References #79.

--- test/system/applicant_views_saved_appliction_test.rb
+++ test/system/applicant_views_saved_appliction_test.rb
@@ -3,13 +3,44 @@ require 'application_system_test_case'
 class ApplicantViewsSavedApplicationTest < ApplicationSystemTestCase
 
   include Devise::Test::IntegrationHelpers
+  include ActionView::RecordIdentifier
 
-  test 'applicant views their saved application' do
-    application = applications(:first)
+  test 'applicant views their saved unsubmitted application' do
+    application = applications(:second)
     sign_in(application.applicant)
     visit scholarships_path
-    click_on 'View Application', match: :first
+    within "##{dom_id(application.scholarship)}" do
+      click_on 'View Application'
+    end
     assert_text application.to_s
+  end
+
+  test 'applicant sees a link to edit a saved unsubmitted application' do
+    application = applications(:second)
+    sign_in(application.applicant)
+    visit scholarship_application_path(application.scholarship, application)
+    assert_link 'Edit'
+  end
+
+  test 'applicant sees a link to delete a saved unsubmitted application' do
+    application = applications(:second)
+    sign_in(application.applicant)
+    visit scholarship_application_path(application.scholarship, application)
+    assert_link 'Delete'
+  end
+
+  test 'applicant does not see a link to edit a submitted application' do
+    application = applications(:first)
+    sign_in(application.applicant)
+    visit scholarship_application_path(application.scholarship, application)
+    refute_link 'Edit'
+  end
+
+  test 'applicant does not see a link to delete a submitted application' do
+    application = applications(:first)
+    sign_in(application.applicant)
+    visit scholarship_application_path(application.scholarship, application)
+    refute_link 'Delete'
   end
 
 end
