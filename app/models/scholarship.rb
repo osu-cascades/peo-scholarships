13b26f4e2bda3955379dc2c7e663a480dae7ea12
Yong Bakos <ybakos@humanoriented.com>
2021-02-03 13:35:37 -0800

    Scholarship: Refine deadline validations.
    The existing validator ensures a scholarship's application_deadline cannot be
    in the past. But, there could be cases where a change is made to a scholarship
    after the deadline.
    
    Refine the existing application_deadline validation to ensure that the
    date cannot be in the past when created only.
    Add a new validator that ensures an existing scholarship with a deadline
    in the past can be updated, but only if the application_deadline isn't
    changed.

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -5,7 +5,8 @@ class Scholarship < ApplicationRecord
 
   validates :name, presence: true
   validates :application_deadline, presence: true
-  validate :application_deadline_cannot_be_in_the_past
+  validate :application_deadline_cannot_be_in_the_past, on: :create
+  validate :application_deadline_cannot_be_changed_to_a_past_date, on: :update
 
   scope :published, -> { where(published: true) }
 
@@ -15,6 +16,12 @@ class Scholarship < ApplicationRecord
 
   def application_deadline_cannot_be_in_the_past
     if application_deadline.present? && application_deadline < Date.today
+      errors.add(:application_deadline, "can't be in the past")
+    end
+  end
+
+  def application_deadline_cannot_be_changed_to_a_past_date
+    if application_deadline_changed? && application_deadline < Date.today
       errors.add(:application_deadline, "can't be in the past")
     end
   end
