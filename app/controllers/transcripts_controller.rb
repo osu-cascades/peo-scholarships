74061a3ebd7775a0eb29d7998c43e3ee419c469b
Taylor Slye <slyet@oregonstate.edu>
2021-02-24 22:53:55 -0800

    transcripts controller now searches the users applications instead of all the applications when looking for transcripts to get rid of references #87

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -2,7 +2,7 @@ class TranscriptsController < ApplicationController
 
   def destroy
     @scholarship = Scholarship.find(params[:scholarship_id])
-    @application = Application.find(params[:application_id])
+    @application = current_user.applications.find(params[:application_id])
     @attachment = @application.transcripts.find_by(params[:trancript_id])
     @attachment.purge
     redirect_to edit_scholarship_application_path(@scholarship, @application)
