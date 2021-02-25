828cf81152e991080d967f919f0f8073c415585d
Taylor Slye <slyet@oregonstate.edu>
2021-02-24 23:07:19 -0800

    transcrips controller now opens the scholarship before finding to make sure it isnt pst deadling references #87

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -1,9 +1,9 @@
 class TranscriptsController < ApplicationController
 
   def destroy
-    @scholarship = Scholarship.find(params[:scholarship_id])
+    @scholarship = Scholarship.open.find(params[:scholarship_id])
     @application = current_user.applications.find(params[:application_id])
-    @attachment = @application.transcripts.find_by(params[:trancript_id])
+    @attachment = @application.transcripts.find(params[:transcript_id])
     @attachment.purge
     redirect_to edit_scholarship_application_path(@scholarship, @application)
   end
