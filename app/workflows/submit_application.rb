6ce3311fee1e5f9859fd7440c7e8610f7c3d5a29
Yong Bakos <ybakos@humanoriented.com>
2021-04-04 22:40:46 -0700

    SubmitApplication: Test base cases of workflow.
    References #101.

--- app/workflows/submit_application.rb
+++ app/workflows/submit_application.rb
@@ -1,12 +1,14 @@
 class SubmitApplication
 
-  def initialize(application, experiences, awards)
+  def initialize(application, experiences = [], awards = [])
     @application = application
     @experiences = experiences
     @awards = awards
   end
 
   def run
+    @application.submitted = true
+    @application.save
     # Hmmm...
   end
 
--- test/workflows/submt_appliction_test.rb
+++ test/workflows/submt_appliction_test.rb
@@ -8,4 +8,19 @@ class SubmitApplicationTest < ActiveSupport::TestCase
     awards = []
     SubmitApplication.new(application, experiences, awards)
   end
+
+  test 'an invalid application fails' do
+    application = Application.new(name: 'Incomplete and Invalid')
+    workflow = SubmitApplication.new(application)
+    refute workflow.run
+  end
+
+  test 'a valid application with no experiences or awards succeeds' do
+    application = applications(:second_unsubmitted)
+    experiences = []
+    awards = []
+    workflow = SubmitApplication.new(application, experiences, awards)
+    assert(workflow.run)
+  end
+
 end
