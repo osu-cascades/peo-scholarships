072b33f55bc43dd633f0e3170615fb4485db10a6
Amber Kolar <amberj.kolar@gmail.com>
2021-04-22 15:51:32 -0700

    Test in system tests that applicant does not see links to edit or submit late, unsubmitted applications References #112

--- test/system/applicant_views_saved_appliction_test.rb
+++ test/system/applicant_views_saved_appliction_test.rb
@@ -7,7 +7,7 @@ class ApplicantViewsSavedApplicationTest < ApplicationSystemTestCase
 
   test 'applicant views their saved unsubmitted application' do
     application = applications(:second_unsubmitted)
-    sign_in(application.applicant)
+    sign_in application.applicant
     visit scholarships_path
     within "##{dom_id(application.scholarship)}" do
       click_on 'View Application'
@@ -17,37 +17,51 @@ class ApplicantViewsSavedApplicationTest < ApplicationSystemTestCase
 
   test 'applicant sees a link to edit a saved unsubmitted application' do
     application = applications(:second_unsubmitted)
-    sign_in(application.applicant)
+    sign_in application.applicant
     visit scholarship_application_path(application.scholarship, application)
     assert_link 'Edit'
   end
 
   test 'applicant sees a link to delete a saved unsubmitted application' do
     application = applications(:second_unsubmitted)
-    sign_in(application.applicant)
+    sign_in application.applicant
     visit scholarship_application_path(application.scholarship, application)
     assert_link 'Delete'
   end
 
   test 'applicant does not see a link to edit a submitted application' do
     application = applications(:first_submitted)
-    sign_in(application.applicant)
+    sign_in application.applicant
     visit scholarship_application_path(application.scholarship, application)
     refute_link 'Edit'
   end
 
   test 'applicant does not see a link to submit a submitted application' do
     application = applications(:first_submitted)
-    sign_in(application.applicant)
+    sign_in application.applicant
     visit scholarship_application_path(application.scholarship, application)
     refute_link 'Submit'
   end
 
   test 'applicant does not see a link to delete a submitted application' do
     application = applications(:first_submitted)
-    sign_in(application.applicant)
+    sign_in application.applicant
     visit scholarship_application_path(application.scholarship, application)
     refute_link 'Delete'
+  end
+
+  test 'applicant does not see a link to edit a late, unsubmitted application' do
+    application = applications(:late)
+    sign_in application.applicant
+    visit scholarship_application_path(application.scholarship, application)
+    refute_link 'Edit'
+  end
+
+  test 'applicant does not see a link to submit a late, unsubmitted application' do
+    application = applications(:late)
+    sign_in application.applicant
+    visit scholarship_application_path(application.scholarship, application)
+    refute_link 'Submit'
   end
 
 end
