deebe9bfd75336ea96b1570058de4bfb1fb83e6f
Amber Kolar <amberj.kolar@gmail.com>
2021-04-22 14:34:01 -0700

    Add system test that admin does not see links to edit published scholarships from scholarship list References #60

--- test/system/admin_edits_scholarship_test.rb
+++ test/system/admin_edits_scholarship_test.rb
@@ -33,6 +33,18 @@ class AdminEditsScholarshipTest < ApplicationSystemTestCase
     assert_text 'Edited Fake Scholarship'
   end
 
+  test 'admin does not see links to edit published scholarships from scholarship list' do
+    sign_in users(:admin)
+    visit admin_scholarship_path(scholarships(:unpublished))
+    click_link 'publish'
+    visit admin_scholarship_path(scholarships(:unpublished_past_deadline))
+    click_link 'publish'
+
+    visit admin_scholarships_path
+    refute_link 'Edit'
+    assert_text 'Can Not Edit'
+  end
+
   test 'admin does not see a link to edit a published scholarship on its page' do
     visit admin_scholarship_path(scholarships(:first))
     refute_link 'Edit'
