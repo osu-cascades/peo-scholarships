40a59f76fbc82592c050c7fa58ea9b6975f619e4
Amber Kolar <amberj.kolar@gmail.com>
2021-04-22 15:09:10 -0700

    Correct skipped system test for applicant editing a saved application to use an unsubmitted application References #112

--- test/system/applicant_edits_saved_application_test.rb
+++ test/system/applicant_edits_saved_application_test.rb
@@ -5,9 +5,8 @@ class ApplicantEditsSavedApplicationTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
 
   test 'applicant edits their saved application' do
-    skip # Need to resolve saving application without validating associated awards
-    application = applications(:first_submitted)
-    sign_in(application.applicant)
+    application = applications(:second_unsubmitted)
+    sign_in application.applicant
     visit edit_scholarship_application_path(application.scholarship, application)
     fill_in 'Address', with: 'New Fake address'
     click_on 'Save Application'
