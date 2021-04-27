3d343c5fa19d377833af60782ac54434a2fd906e
Taylor Slye <slyet@oregonstate.edu>
2021-04-27 13:06:22 -0700

    remove the validations on experiences so a partial can be saved

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -3,11 +3,9 @@ class Experience < ApplicationRecord
   enum kind: [:employment, :volunteer_experience, :school_activity]
   attribute :kind, :integer, default: :employment
 
-  validates_presence_of :title
-  validates_presence_of :started_at
-  validates_presence_of :frequency
-  validates_numericality_of :total_hours
-  validates_presence_of :responsibilities_accomplishments_honors
+  def to_s
+    title
+  end
 
   belongs_to :application
 
