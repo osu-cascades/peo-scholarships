108a004cab1b82aba5088781a570053d5488bb4a
Taylor Slye <slyet@oregonstate.edu>
2021-04-27 13:10:05 -0700

    added the _destroy to the params for the application for experiences

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -102,7 +102,7 @@ class ApplicationsController < ApplicationController
       :savings, :investments, :school_loan_debt, :other_debt, :recommendation_letter, transcripts: [],
       answers_attributes: [:id, :question_id, :body],
       awards_attributes: [:id, :name, :date, :description, :_destroy]),
-      experiences_attributes: [:id, :kind, :title, :started_at, :ended_at, :frequency, :hours_per_week, :total_hours, :responsibilities_accomplishments_honors]
+      experiences_attributes: [:id, :kind, :title, :started_at, :ended_at, :frequency, :hours_per_week, :total_hours, :responsibilities_accomplishments_honors, :_destroy]
   end
 
 end
