8ab146ca452c6e28af62c23372d9d3328d5b7d59
Amber Kolar <amberj.kolar@gmail.com>
2021-04-29 16:11:52 -0700

    Update system test for admin making new question to include word limit References #81

--- test/system/admin_adds_question_to_scholarship_test.rb
+++ test/system/admin_adds_question_to_scholarship_test.rb
@@ -5,13 +5,15 @@ class AdminAddsQuestionToScholarshipTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
 
   test 'admin adds question to scholarship' do
-    sign_in(users(:admin))
+    sign_in users(:admin)
     visit admin_scholarship_path(scholarships(:first))
     click_link 'hide'
     click_on 'Add New Question'
     fill_in 'Prompt', with: 'Fake New Question'
+    fill_in 'Word Limit', with: '100'
     click_button 'Create Question'
     assert_text 'Question added'
     assert_text 'Fake New Question'
+    assert_text '100'
   end
 end
