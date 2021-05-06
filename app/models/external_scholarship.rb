5877fb0195f7dfbfd1e5bc8e0a55fa820c6007c8
Taylor Slye <slyet@oregonstate.edu>
2021-05-06 14:06:19 -0700

    made model for external scholarship

--- app/models/external_scholarship.rb
+++ app/models/external_scholarship.rb
@@ -1,2 +1,11 @@
 class ExternalScholarship < ApplicationRecord
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: true
+
+  def to_s
+    name
+  end
+
+  belongs_to :application
 end
