3d6a37ced903cf81158f9a04775b38464cd6bacb
Taylor Slye <slyet@oregonstate.edu>
2021-05-18 14:06:45 -0700

    test application controller redirect after sign in

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -3,5 +3,11 @@ require 'test_helper'
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
+  test 'redirects after applicant sign in' do
+    skip
+    applicant = users(:applicant)
+    sign_in(applicant)
+    assert_redirected_to scholarships_path
+  end
 
 end
