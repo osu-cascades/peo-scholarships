63de8a5983753903a70019eea7ae41fff0a259a8
Taylor Slye <slyet@oregonstate.edu>
2021-05-20 13:47:31 -0700

    create awards attributes tests for model

--- test/models/award_test.rb
+++ test/models/award_test.rb
@@ -2,6 +2,13 @@ require 'test_helper'
 
 class AwardTest < ActiveSupport::TestCase
 
+  test 'attributes' do
+    award = Award.new
+    assert_respond_to(award, :name)
+    assert_respond_to(award, :description)
+    assert_respond_to(award, :date)
+  end
+
   test 'belongs to an application' do
     assert_respond_to(Award.new, :application)
   end
