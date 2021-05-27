07b0a5c74c1bc0f608faf7542568a6fefabeed1d
Amber Kolar <amberj.kolar@gmail.com>
2021-05-27 14:45:06 -0700

    Stub out a mailer to request recommendation letters References #43

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  # # See issue 43
+  # def recommendation_letter_email user
+  #   default from: 'from@example.com'
+  #   layout 'mailer'
+  #   @user = user
+  #   mail(:to => 'to@example.com', :subject => 'Recommendation Letter Requested')
+  # end
+end
