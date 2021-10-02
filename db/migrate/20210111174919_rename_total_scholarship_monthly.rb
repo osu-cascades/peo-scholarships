7b72dbb8daffd96bf200383e9bbdbbe5897e39fc
Yong Bakos <ybakos@humanoriented.com>
2021-10-01 22:47:56 -0700

    Migrations: Fix class name.
    The file was renamed but the class was not.

--- db/migrate/20210111174919_rename_total_scholarship_monthly.rb
+++ db/migrate/20210111174919_rename_total_scholarship_monthly.rb
@@ -1,4 +1,4 @@
-class RenameTotalScholarshipMonthy < ActiveRecord::Migration[5.2]
+class RenameTotalScholarshipMonthly < ActiveRecord::Migration[5.2]
   def change
     rename_column :applications, :total_scholarship_monthy, :total_scholarship_monthly
   end
