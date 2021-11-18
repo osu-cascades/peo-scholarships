ffff4542ec6b7547e5a8422a5a84e578d4137cb3
Yong Bakos <ybakos@humanoriented.com>
2021-11-17 16:29:00 -0800

    Award: Move validations above method implementations.

--- app/models/award.rb
+++ app/models/award.rb
@@ -1,13 +1,13 @@
 class Award < ApplicationRecord
 
   belongs_to :application
-  
-  def to_s
-    name
-  end
 
   validates :name, presence: true
   validates :description, presence: true
   validates :date, presence: true
+  
+  def to_s
+    name
+  end
 
 end
